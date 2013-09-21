02e7a117071d388320241bb5ec0dc162530be345
Charlie Swanson <cswanson310@gmail.com>
2013-09-21 12:54:03 -0400

    first version of facebook

--- niplistProject/niplist/static/assets/js/homepage.js
+++ niplistProject/niplist/static/assets/js/homepage.js
@@ -1,10 +1,15 @@
 window.onload = function(){
   $('#login').bind('click', function(e){
     e.preventDefault();
-    $('#login-container').removeClass('hidden');
-    $('#overlay').addClass('show');
-    $('.form-signin input[type=text], .form-signin input[type=password]').addClass('form-control'); //bootstrap styling
-    $('.form-signin input[type=text]').attr('placeholder', 'E-mail');
-    $('.form-signin input[type=password]').attr('placeholder', 'Password');
+    FB.login(function(response) {
+      if (response.authResponse) {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+          console.log('Good to see you, ' + response.name + '.');
+        });
+      } else {
+        console.log('User cancelled login or did not fully authorize.');
+      }
+    });
   })
 }
